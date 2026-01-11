6335e4503cd7e0fed145493435e6bc68f0f2f700
Nibir00795 <khanshuvo7444@gmail.com>
2026-01-11 16:46:47 -0500

    Initial Commit

---
+++ Timer FlashLight/Timer_FlashLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  Timer_FlashLightApp.swift
+//  Timer FlashLight
+//
+//  Created by Md Jonayed Hossain Chowdhury on 1/11/26.
+//
+
+import SwiftUI
+
+@main
+struct Timer_FlashLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
